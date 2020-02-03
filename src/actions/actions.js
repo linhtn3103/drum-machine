7c5019c38c125891a8acecd379cd7d3f909ead5c
LinhTN-3103 <trannhatlinh3103@gmail.com>
2020-02-03 10:38:50 +0700

    action
    create action

---
+++ src/actions/actions.js
@@ -0,0 +1,30 @@
+export const POWER = 'POWER';
+export const VOLUME = 'VOLUME';
+export const DISPLAY = 'DISPLAY';
+export const BANK = 'BANK';
+
+export const changePower = currentState => {
+  return {
+    type: POWER,
+    power: !currentState
+  };
+};
+
+export const changeVolume = newVolume => {
+  return {
+    type: VOLUME,
+    volume: newVolume
+  };
+};
+export const changeDisplay = newDisplay => {
+  return {
+    type: DISPLAY,
+    display: newDisplay
+  };
+};
+export const changeBank = currentBank => {
+  return {
+    type: BANK,
+    bank: !currentBank
+  };
+};
